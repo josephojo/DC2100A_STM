dde77e9f2a98262020ed043befd0a31a6c45f766
Joseph O <36710290+josephojo@users.noreply.github.com>
2020-08-23 13:31:54 -0400

    Removing the temperature task since it isn't currently being used

--- DC2100A.cpp
+++ DC2100A.cpp
@@ -276,7 +276,7 @@ int main()
     mainQueue.call_every(BALANCER_TASK_RATE,        Task_Balancer);
     mainQueue.call_every(VOLTAGE_TASK_RATE,         Task_Voltage); // Used to me 100ms now 200ms
     //mainQueue.call_every(PACK_CURRENT_TASK_RATE,    Task_Pack_Current);
-    mainQueue.call_every(TEMPERATURE_TASK_RATE,     Task_Temperature);
+    //mainQueue.call_every(TEMPERATURE_TASK_RATE,     Task_Temperature);
     mainQueue.call_every(USB_TASK_RATE,             Task_Parser);
     mainQueue.call_every(STATUS_TASK_RATE,          Task_Status);
     mainQueue.call_every(DETECT_TASK_RATE,          Task_Detect);
